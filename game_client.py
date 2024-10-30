96d5832150321672902c507fe2731cf4233ac675
Habeeb Hussain Al Hamed <habeeb403020@gmail.com>
2024-10-30 06:25:06 +0530

    Initial Commit: Basic movement (in sync) and rendering

--- game_client.py
+++ game_client.py
@@ -1,5 +1,5 @@
 
-import asyncio, json, random
+import asyncio, json, random, time
 from collections import defaultdict
 import threading as t
 import websockets
@@ -10,11 +10,17 @@ class GameClient:
     uri = ""
     server_socket = None
     game_state = defaultdict(dict)
+    local_client_state = dict()
     pid = ""
-    retry_pattern = lambda x: 2*x #the next amt of time to wait
-    latency = 0.0 #in ms    
+    #the next amt of time to wait before retrying
+    retry_pattern = lambda x: (0.01+x) if x < 3 else 0.001
+    latency = -1 #in ms (-1 indicates retrying connection
+    #-2 means connected but data is not being transferred yet)
+    stop_flag = False #used to stop the indefinite retry loop
+    sync_flag = False #ask the local client to refresh
 
-    def __init__(self, pid=None, retry_pattern=None):
+    def __init__(self, uri, pid=None, retry_pattern=None):
+        GameClient.uri = uri
         if pid is None:
             pid = str(random.randint(int(1e9), int(1e12)))
         GameClient.pid = pid
@@ -23,26 +29,68 @@ class GameClient:
 
     @staticmethod
     async def connect(ping_delay=20):
+        #set retry status in latency var
+        GameClient.latency = -1
         try:
             GameClient.server_socket = \
             await websockets.connect(GameClient.uri,
-                                     ping_interval=ping_delay)
-        except:
+                                     ping_interval=ping_delay/1000)
+            GameClient.latency = -2 #set success status
+        except Exception as e:
+            print(f"\nError[connect]: {e}\n")
             GameClient.server_socket = None
 
     @staticmethod
-    async def exchange_states():
+    async def exchange_states(data=None):
 
-        message = GameClient.game_state[
-            GameClient.pid
-        ]
-        message["id"] = GameClient.pid
+        s = time.time()
+
+        if data is None:
+            message = dict(GameClient.local_client_state)
+            message["id"] = GameClient.pid
+        else:
+            message = data
         await GameClient.server_socket.send(
             json.dumps(message)
         )
+        print("Debug: Local client sent")
 
         recv_state = await GameClient.server_socket.recv()
+        print("Debug: Local client recv")
         recv_state = json.loads(recv_state)
         GameClient.game_state = recv_state
+        GameClient.sync_flag = True #ask players to sync
 
-    
+        GameClient.latency = round(time.time() - s, 3) * 1000
+
+    @staticmethod
+    async def retry_exchange(timeout=None):
+
+        time_taken = 0
+        wait_time = 0.001 #start waiting from 1 sec
+
+        while (timeout is None or time_taken + wait_time \
+            <= timeout) and not(GameClient.stop_flag):
+
+            try:
+                await GameClient.exchange_states()
+                #reset time vars
+                time_taken = 0
+                wait_time = 0.001
+            except Exception as e:
+                print(f"\nError[retry]: {e}\nRetrying in \
+{wait_time*1000} ms...")
+                await asyncio.sleep(wait_time)
+                time_taken += wait_time
+                wait_time = GameClient.retry_pattern(
+                    wait_time
+                )
+
+                await GameClient.connect()
+                print("Debug:", GameClient.latency)
+        else:
+            if GameClient.stop_flag:
+                try:
+                    await GameClient.server_socket.close()
+                except:
+                    pass
--- game_objects.py
+++ game_objects.py
@@ -1,5 +1,5 @@
 
-
+import random
 import pygame
 from game_client import GameClient
 
@@ -20,7 +20,7 @@ class DataItems:
 
 class RenderItems:
 
-    font = pygame.font.Font(pygame.font.SysFont, 36)
+    font = pygame.font.Font(None, 36)
 
 
 class PlayerCharacter:
@@ -28,52 +28,40 @@ class PlayerCharacter:
     """A (replicable) player character with boundary checks.
     Rendered as a circle with a name floating on top"""
 
-    def __init__(self, x, y, radius, pid, color):
+    def __init__(self, x, y, radius, pid):
         self.x, self.y = x, y
         self.radius = radius
         self.pid = pid
-        self.color = color
+        self.color = random.choice(list(
+            DataItems.colors.keys()))
 
         self.name = RenderItems.font.render(
-            self.pid, True, self.color
+            self.pid, True,
+            DataItems.colors[self.color]
         )
-
-        #set initial local client data
-        GameClient.local_client_state['x'] = self.x
-        GameClient.local_client_state['y'] = self.y
-        GameClient.local_client_state["color"] = self.color
-
-    def boundary_check(self):
-
-        """Checks for boundary penetrations and resets"""
-        if self.x < self.radius:
-            self.x = self.radius
-        elif self.x > 1000 - self.radius:
-            self.x = 1000 - self.radius
-        
-        if self.y < self.radius:
-            self.y = self.radius
-        elif self.y > 650 - self.radius:
-            self.y = 650 - self.radius
 
     def update(self, events: list[pygame.event.Event]):
 
         """Called every frame"""
 
-        #boundary check
-        self.boundary_check()
-
         #update from global state
         #for the current client it can be used for syncing
         #for other client replicas it is their state
         try:
-            self.x = GameClient.game_state[self.pid]['x']
-            self.y = GameClient.game_state[self.pid]['y']
+            if GameClient.sync_flag:
+                self.x = GameClient.game_state[self.pid]['x']
+                self.y = GameClient.game_state[self.pid]['y']
+                self.color = GameClient.game_state[self.pid]["color"]
+                print(f"DEBUG: LOCAL: \
+{GameClient.local_client_state}\n GLOBAL: \
+{GameClient.game_state[self.pid]}")
+            print("Debug: Skipping sync")
         except KeyError:
             pass
 
     def draw(self, surf):
 
+        #TODO: Draw dynamic sea urchin textures
         pygame.draw.circle(surf, self.color, (self.x, self.y),
                            self.radius) #body
         pygame.draw.circle(surf, (0, 0, 0), (self.x, self.y),
@@ -97,6 +85,27 @@ class PawnCharacter(PlayerCharacter):
 
     speed = 15 #movement speed
 
+    def __init__(self, x, y, radius, pid):
+        super().__init__(x, y, radius, pid)
+
+        #set initial local client data
+        GameClient.local_client_state['x'] = self.x
+        GameClient.local_client_state['y'] = self.y
+        GameClient.local_client_state["color"] = self.color
+    
+    def boundary_check(self):
+
+        """Checks for boundary penetrations and resets"""
+        if self.x < self.radius:
+            self.x = self.radius
+        elif self.x > 1000 - self.radius:
+            self.x = 1000 - self.radius
+        
+        if self.y < self.radius:
+            self.y = self.radius
+        elif self.y > 650 - self.radius:
+            self.y = 650 - self.radius
+
     def update(self, events: list[pygame.event.Event]):
 
         super().update(events)
@@ -106,30 +115,33 @@ class PawnCharacter(PlayerCharacter):
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
                     PawnCharacter.key_w = True
-                elif event.key == pygame.K_s:
+                if event.key == pygame.K_s:
                     PawnCharacter.key_s = True
                 if event.key == pygame.K_a:
                     PawnCharacter.key_a = True
-                elif event.key == pygame.K_d:
+                if event.key == pygame.K_d:
                     PawnCharacter.key_d = True
-            elif event.type == pygame.KEYUP:
+            if event.type == pygame.KEYUP:
                 if event.key == pygame.K_w:
                     PawnCharacter.key_w = False
-                elif event.key == pygame.K_s:
+                if event.key == pygame.K_s:
                     PawnCharacter.key_s = False
                 if event.key == pygame.K_a:
                     PawnCharacter.key_a = False
-                elif event.key == pygame.K_d:
+                if event.key == pygame.K_d:
                     PawnCharacter.key_d = False
         
         if PawnCharacter.key_w:
-            self.y += PawnCharacter.speed
-        elif PawnCharacter.key_s:
             self.y -= PawnCharacter.speed
+        if PawnCharacter.key_s:
+            self.y += PawnCharacter.speed
         if PawnCharacter.key_a:
             self.x -= PawnCharacter.speed
-        elif PawnCharacter.key_d:
+        if PawnCharacter.key_d:
             self.x += PawnCharacter.speed
+
+        #boundary check
+        self.boundary_check()
         
         #update local client data for sending to server
         GameClient.local_client_state['x'] = self.x
